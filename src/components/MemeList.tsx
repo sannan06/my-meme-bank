65919c8a5b2842f5cd2fb04ff2fd28f977b64ece
sannan06 <sannanhafeez06@gmail.com>
2018-11-18 14:35:30 +1300

    added voice recognition

--- src/components/MemeList.tsx
+++ src/components/MemeList.tsx
@@ -1,4 +1,6 @@
+import MediaStreamRecorder from 'msr';
 import * as React from "react";
+
 
 interface IProps {
     memes: any[],
@@ -7,9 +9,11 @@ interface IProps {
 }
 
 export default class MemeList extends React.Component<IProps, {}> {
+
     constructor(props: any) {
         super(props)   
         this.searchByTag = this.searchByTag.bind(this)
+        this.PostAudio = this.PostAudio.bind(this)
     }
 
 	public render() {
@@ -19,6 +23,7 @@ export default class MemeList extends React.Component<IProps, {}> {
                     <div className="input-group">
                         <input type="text" id="search-tag-textbox" className="form-control" placeholder="Search By Tags" />
                         <div className="input-group-append">
+                            <div className="btn" onClick={this.searchTagByVoice}><i className="fa fa-microphone" /></div>
                             <div className="btn btn-outline-secondary search-button" onClick = {this.searchByTag}>Search</div>
                         </div>
                     </div>  
@@ -71,4 +76,65 @@ export default class MemeList extends React.Component<IProps, {}> {
         this.props.searchByTag(tag)  
     }
 
+    private searchTagByVoice(){
+
+        const mediaConstraints = {
+            audio: true
+        }
+        const onMediaSuccess = (stream: any) => {
+            const mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+            mediaRecorder.ondataavailable = (blob: any) => {
+                this.PostAudio(blob);
+                mediaRecorder.stop()
+            }
+            mediaRecorder.start(3000);
+        }
+    
+        navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError)
+    
+        function onMediaError(e: any) {
+            console.error('media error', e);
+        }
+    }
+
+    private PostAudio(blob: any) {
+        let accessToken: any;
+        fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0', {
+            headers: {
+                'Content-Length': '0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Ocp-Apim-Subscription-Key': '7b49b5562f2e40e786f6e54e7163365a'
+            },
+            method: 'POST'
+        }).then((response) => {
+            // console.log(response.text())
+            return response.text()
+        }).then((response) => {
+            console.log(response)
+            accessToken = response
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+
+        // posting audio
+        fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken', {
+            body: blob, // this is a .wav audio file    
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer' + accessToken,
+                'Content-Type': 'audio/wav;codec=audio/pcm; samplerate=16000',
+                'Ocp-Apim-Subscription-Key': '7b49b5562f2e40e786f6e54e7163365a'
+            },    
+            method: 'POST'
+        }).then((res) => {
+            return res.json()
+        }).then((res: any) => {
+            const textBox = document.getElementById("search-tag-textbox") as HTMLInputElement
+            textBox.value = (res.DisplayText as string).slice(0, -1)
+            console.log(res)
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+    }
 }
